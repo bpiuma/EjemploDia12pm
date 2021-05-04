3a7222d4553962455fb2eb009daa1dd47bb84704
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-05-04 03:02:27 +0000

    Ejemplo finalizado

--- src/js/component/abuelo.js
+++ src/js/component/abuelo.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Padre } from "./padre.js";
 
 export function Abuelo() {
-	let apellido = "Perez";
+	const [apellido, setApellido] = useState("Perez");
+	const [edad, setEdad] = useState(80);
+
+	const decirHola = () => {
+		console.log("Hola buenos días");
+	};
+
+	const saludar = frase => {
+		console.log(frase);
+	};
+
+	const cambiarApellido = () => {
+		setApellido("Sepulveda");
+		console.log(apellido);
+	};
+
+	const envejecer = () => {
+		setEdad(prevState => {
+			return prevState + 1;
+		});
+		console.log(edad);
+	};
 
 	return (
 		<div className="red">
-			Soy el abuelo y me apellido es {apellido}
+			Soy el abuelo y me apellido es {apellido} y mi edad es {edad}
+			<button onClick={decirHola}>Saludar1</button>
+			<button
+				onClick={() => {
+					saludar("Buenas noches");
+				}}>
+				Saludar2
+			</button>
+			<button onClick={cambiarApellido}>Cambiar Apellido</button>
+			<button onClick={envejecer}>Envejecer</button>
 			<Padre apellidoFamiliar={apellido} />
 		</div>
 	);
--- src/js/component/hijo.js
+++ src/js/component/hijo.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 export function Hijo(props) {
+	useEffect(() => {
+		console.log(
+			"Yo me ejecuto cuando me monto en la UI o cuando el componente haga update de props o de state"
+		);
+	});
+
+	useEffect(() => {
+		console.log("Yo solo me ejecuto cuando me monto en la UI");
+	}, []);
+
+	useEffect(() => {
+		console.log(
+			"Yo solo me ejecuto cuando me monto en la UI o cuando el prop apellidoFamiliar cambia"
+		);
+	}, [props.apellidoFamiliar]);
+
 	return (
 		<div className="blue">
 			Soy el hijo, hijo del padre y mi apellido es{" "}
